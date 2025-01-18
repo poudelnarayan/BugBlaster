52b19e1f8642a8a7e870adede00f784b281dfb57
Narayan Poudel <poudelnarayan434@GMAIL.COM>
2025-01-18 21:03:21 +0545

    added form UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-function App() {
+import TicketForm from "./components/TicketForm";
+
+const App = () => {
   return (
     <div className="App">
-      <div className="container"></div>
+      <div className="container">
+        <h1>Bug Blaster</h1>
+        <TicketForm />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -17,7 +17,51 @@ const TicketForm = () => {
     setPriority("1");
   };
 
-  return <></>;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    clearForm();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ticket-form">
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          className="form-input"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Description</label>
+        <textarea
+          type="text"
+          className="form-input"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+      </div>
+      <fieldset className="priority-fieldset">
+        <legend>Priority</legend>
+        {Object.entries(priorityLabels).map(([key, value]) => (
+          <label className="priority-label" key={key}>
+            <input
+              type="radio"
+              value={key}
+              checked={priority === key}
+              className="priority-input"
+              onChange={(e) => setPriority(e.target.value)}
+            />
+            {value}
+          </label>
+        ))}
+      </fieldset>
+      <button type="submit" className="button">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default TicketForm;
